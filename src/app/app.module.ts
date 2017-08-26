d01a624d03d81477d6f7693f543b8d43ae5a74b4
OlmerPL <30810593+OlmerPL@users.noreply.github.com>
2017-08-26 21:09:38 +0200

    control panel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ControlPanelComponent } from './control-panel/control-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ControlPanelComponent
   ],
   imports: [
     BrowserModule
