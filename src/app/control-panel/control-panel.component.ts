1160e1ad915d303942a287a59e5633b36d6a9117
OlmerPL <30810593+OlmerPL@users.noreply.github.com>
2017-08-26 22:37:47 +0200

    generation of data

--- src/app/control-panel/control-panel.component.ts
+++ src/app/control-panel/control-panel.component.ts
@@ -5,9 +5,52 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './control-panel.component.html',
   styleUrls: ['./control-panel.component.css']
 })
-export class ControlPanelComponent implements OnInit {
 
-  constructor() { }
+export class ControlPanelComponent implements OnInit {
+  public dataValue: number;
+
+  private dataTable: number[];
+
+  constructor() {
+    this.dataValue = 10;
+    this.dataTable = [];
+  }
+
+  static shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  fillDataTable(event) {
+    this.dataValue = event.target.value;
+  }
+
+  initialDataTableContent() {
+    this.dataTable.length = 0;
+    let i: number;
+    for (i = 0; i < this.dataValue; i++) {
+      this.dataTable.push(i + 1);
+    }
+    console.log(this.dataTable);
+  }
+
+  randomizeDataTable() {
+    this.dataTable = this.shuffle(this.dataTable);
+    console.log(this.dataTable);
+  }
 
   ngOnInit() {
   }
